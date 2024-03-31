ab67c70694d048859231ff2865b3adece74513dc
tct123 <42028373+tct123@users.noreply.github.com>
2024-03-31 19:41:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import flet as ft
+import dotenv as dv
+import os
+
+dv.load_dotenv()
+url = os.getenv("TAXOMETERAPIURL")
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.adaptive = True
+    page.appbar = ft.AppBar(title=ft.Text(value="Taxometer"))
+    body = ft.SafeArea(ft.Text("Hello, Flet!"))
+    page.add(body)
 
 
 ft.app(main)
